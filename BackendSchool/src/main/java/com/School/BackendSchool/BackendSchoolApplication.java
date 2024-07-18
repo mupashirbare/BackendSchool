d98ee4de450e0fe23902befa72ffb3dcb37e439c
Mubashir <mubashirbarre12@gmail.com>
2024-07-18 08:18:13 -0700

    Add files via upload

---
+++ BackendSchool/src/main/java/com/School/BackendSchool/BackendSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.School.BackendSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSchoolApplication.class, args);
+	}
+
+}
---
+++ BackendSchool/src/test/java/com/School/BackendSchool/BackendSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.School.BackendSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
